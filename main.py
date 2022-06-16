4f3706cf01a182fb3e65db3a890972129c997907
blacktr1007 <ohs1192kou@gmail.com>
2022-06-16 22:48:07 +0900

    update main function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import yaml
 from dacite.core import from_dict
+from flask.wrappers import Request
 from pysesame3.auth import WebAPIAuth
 from pysesame3.chsesame2 import CHSesame2
 
@@ -7,7 +8,7 @@ from config.config import ConfigSchema
 from open_sesame.core import OpenSesame
 
 
-def create_oepn_sesame() -> OpenSesame:
+def main(request: Request):
     config_file = "/etc/secrets/config.yml"
     with open(config_file, "r") as config_fp:
         config = from_dict(data_class=ConfigSchema, data=yaml.safe_load(config_fp))
@@ -19,14 +20,11 @@ def create_oepn_sesame() -> OpenSesame:
         secret_key=config.sesame.secret_key,
     )
 
-    return OpenSesame(device=device, history_tag=config.history_tag)
+    sesame: OpenSesame = OpenSesame(device=device, history_tag=config.history_tag)
 
+    action: str = request.args.get("action", "")
 
-def lock():
-    sesame = create_oepn_sesame()
-    sesame.lock()
-
-
-def unlock():
-    sesame = create_oepn_sesame()
-    sesame.unlock()
+    if action == "lock":
+        sesame.lock()
+    elif action == "unlock":
+        sesame.unlock()
