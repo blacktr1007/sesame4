6ac9db643f57cf3ffcb557a8a9640677c85d4f4f
blacktr1007 <ohs1192kou@gmail.com>
2022-06-27 02:20:05 +0900

    fix timezone

--- open_sesame/core.py
+++ open_sesame/core.py
@@ -1,23 +1,32 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
 
+import pytz
 from pysesame3.chsesame2 import CHSesame2
 from pysesame3.const import CHSesame2ShadowStatus
+from pytz.tzinfo import DstTzInfo, StaticTzInfo
 
 
 @dataclass
 class OpenSesame:
     device: CHSesame2
     history_tag: str
+    location: StaticTzInfo | DstTzInfo | pytz._UTCclass = field(init=False)  # pyright: ignore ReportPrivateUsage
+
+    def __post_init__(self):
+        self.location = pytz.timezone("Asia/Tokyo")
+
+    def _current_date(self) -> str:
+        return self.location.localize(datetime.now()).strftime("%Y/%m/%d %H:%M:%S")
 
     def lock(self) -> str:
         if self.device.getDeviceShadowStatus() == CHSesame2ShadowStatus.LockedWm:
             return "Device is already Locked!"
         self.device.lock(history_tag=self.history_tag)
-        return f"Locked at {datetime.now().strftime('%Y/%m/%d %H:%M:%S')}"
+        return f"Locked at {self._current_date()}"
 
     def unlock(self) -> str:
         if self.device.getDeviceShadowStatus() == CHSesame2ShadowStatus.UnlockedWm:
             return "Device is already Unlocked!"
         self.device.unlock(history_tag=self.history_tag)
-        return f"Unlocked at {datetime.now().strftime('%Y/%m/%d %H:%M:%S')}"
+        return f"Unlocked at {self._current_date()}"
